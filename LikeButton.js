0aa01f43c9624808aa2fd138ee24307915222052
Larhwally <lawalwhylawal@gmail.com>
2020-12-22 00:54:35 +0100

    Added readme file

--- LikeButton.js
+++ LikeButton.js
@@ -1,25 +1,39 @@
 // JavaScript source code
-function incrementValue() {
-    var value = parseInt(document.getElementById('like-counter').value, 10);
+
+const baseURL = 'https://localhost:44313'
+
+async function incrementValue() {
+    let value = parseInt(document.getElementById('like-counter').value, 10);
     value = isNaN(value) ? 0 : value;
     value++;
     document.getElementById('like-counter').value = value;
-    saveCountToDb(value);
+
+    try {
+        await saveCountToDb(value);
+    } catch (error) {
+        console.error(error);
+        alert('An error occured while updating likes');
+    }
 }
 
 async function getLikesCount() {
-    let response = await fetch('https://localhost:44313/api/Likes');
-    let data = await response.json();
-    document.getElementById('like-counter').value = data;
+    try {
+        const response = await fetch(`${baseURL}/api/Likes`);
+        const data = await response.json();
+        document.getElementById('like-counter').value = data;
+    } catch (error) {
+        console.error(error);
+        alert('Could not load the number of likes');
+    }
 }
 
 async function saveCountToDb(value) {
-    fetch('https://localhost:44313/api/Likes', {
+    const response = await fetch(`${baseURL}/api/Likes`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({ likeCount: value }),
-    })
-        .then(response => response.json())
+    });
+    return await response.json();
 }
--- readme.md
+++ readme.md
@@ -12,9 +12,9 @@ Start by cloning the project from the git repository into your local machine and
 ### Installing
 
 After cloning the project to your local machine, some settings has to be changed to run the API locally on your machine as these said settings are working locally on my machine hence the changes.
-- ConnectionString in appSettings.json should be changed to carry your database credentials.
-- launch URL on launchSettings.json should be changed according to the default url the API is running on your local machine.
-- The default url the API is running on your local machine should be used to update the likeButton.js file 
+- `ConnectionString` in `appSettings.json` should be changed to carry your database credentials.
+- launch URL on `launchSettings.json` should be changed according to the default url the API is running on your local machine.
+- Change the `baseURL` variable found in `likeButton.js` to the URL of your local server.
 
 ## Run Test
 - You can use the run button on Visual studio to start up the web API or navigate to the project folder from your CLI and run by typing 'dotnet run'
